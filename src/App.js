e4f8be86ad96247e77f55d3f16fa4f978c2265c9
Waldemar Laskowski <wallas5h@gmail.com>
2020-11-08 18:43:48 +0100

    adding file to github

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Text from './Text'
+
+
+class App extends Component {
+  state = {
+    underline: false
+  }
+  render() {
+    const text = 'Witaj na stronie'
+    // let classes = ''
+    // if (this.state.underline) classes += " line";
+    // if (text.length > 10) classes += ' big'
+
+    const classes = ['one'];
+    if (this.state.underline) classes.push(' line');
+    if (text.length > 10) classes.push(' big');
+
+    return (
+      <div className='app' onClick={() => { this.setState({ underline: !this.state.underline }) }} >
+        <h1 className={classes.join(" ")}>{text}</h1>
+        <Text />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Text.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Text = () => {
+    return (
+        <p className='red'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi praesentium ex cupiditate molestiae neque alias, placeat nam at dolorum natus quo illo, dolores repellat! Fuga dicta nisi facere ex quisquam.</p>
+    );
+}
+
+export default Text;
