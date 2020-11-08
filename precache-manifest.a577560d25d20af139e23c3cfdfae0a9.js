640438be91808eba7b193c65264add4fb94dcaf6
Waldemar Laskowski <wallas5h@gmail.com>
2020-11-08 19:07:49 +0100

    Updates

---
+++ precache-manifest.a577560d25d20af139e23c3cfdfae0a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "80edf7ffb52391219d21",
+    "url": "/react-app/static/css/main.bdb68a33.chunk.css"
+  },
+  {
+    "revision": "80edf7ffb52391219d21",
+    "url": "/react-app/static/js/main.df2bcf64.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "ceb1717918675d33a0f2",
+    "url": "/react-app/static/js/2.e01f3c02.chunk.js"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "4a44c683f678746aae5edff9d1be0916",
+    "url": "/react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.3e10bd54deae237351e857b10b752102.js"
+  "/react-app/precache-manifest.a577560d25d20af139e23c3cfdfae0a9.js"
 );
 
 workbox.clientsClaim();
